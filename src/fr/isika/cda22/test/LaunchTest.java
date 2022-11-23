d81e93a93a83cf33b08f1ecb8d658d7a0577acdc
PAUBEL <paul.paubel93@gmail.com>
2022-11-23 10:29:34 +0100

    Initialisation du git

---
+++ src/fr/isika/cda22/test/LaunchTest.java
@@ -0,0 +1,12 @@
+package fr.isika.cda22.test;
+
+public class LaunchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		
+	}
+
+}
